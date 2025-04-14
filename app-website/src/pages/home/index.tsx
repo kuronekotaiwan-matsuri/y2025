5d8624e61c8acbdae87fb101687320c4dac98b36
yo16 <xxyo1xx@gmail.com>
2025-04-14 10:48:16 +0900

    Navbar7のパラメータ込みでHeaderにした

--- app-website/src/pages/home/index.tsx
+++ app-website/src/pages/home/index.tsx
@@ -1,32 +1,9 @@
-import { Navbar7 } from "../../components/Navbar7";
+import { Header } from "../../components/Header";
 
 export default function Home() {
     return (
         <div>
-            <Navbar7
-                logo={{
-                    src: "https://d22po4pjz3o32e.cloudfront.net/logo-image.svg",
-                    alt: "Logo image"
-                }}
-                links={[
-                    {
-                        title: "お食事・お飲み物ガイド",
-                        url: "/food-drink-guide"
-                    },
-                    {
-                        title: "出店情報",
-                        url: "/shops"
-                    },
-                    {
-                        title: "黒猫台湾まつりについて",
-                        url: "/about"
-                    },
-                    {
-                        title: "お問合せ",
-                        url: "/contact-us"
-                    },
-                ]}
-            />
+            <Header />
         </div>
     )
 }
