57581cc053b9d472674dd76a627bc21f754abd39
yo16 <xxyo1xx@gmail.com>
2025-04-19 01:02:40 +0900

    changed URL

--- app-website/src/utilities/postStamp.ts
+++ app-website/src/utilities/postStamp.ts
@@ -1,11 +1,13 @@
 // スタンプ情報を、GASサーバーへ非同期でPOSTする
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwdtw-0f-cucTO7XFCVeCuLLiof5b9mmu5U89tu1w-xabkNNy8Dv-6vso5R6A60u2tI/exec";
+//const GAS_URL12 = "https://script.google.com/macros/s/AKfycbwdtw-0f-cucTO7XFCVeCuLLiof5b9mmu5U89tu1w-xabkNNy8Dv-6vso5R6A60u2tI/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxMBCCxug018E3fb9V0dEVGRd2_wjwfliGkcRqN0olQVqPEcpk-8knmVQoMA6hW6P15/exec";
+
 
 export const postStamp = (userId: string, stampId: string) => {
     const data = {
-        userId: userId,
-        stampId: stampId
+        uuid: userId,
+        stamp_id: stampId
     };
     fetch(GAS_URL, {
         method: "POST",
