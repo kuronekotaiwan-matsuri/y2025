51456ef6ff78ed80bd9a74ae7c9beeed40dd971e
yo16 <xxyo1xx@gmail.com>
2025-04-19 15:30:53 +0900

    changed URL to GCP-CF

--- app-website/src/utilities/postStamp.ts
+++ app-website/src/utilities/postStamp.ts
@@ -3,9 +3,10 @@
 //const GAS_URL12 = "https://script.google.com/macros/s/AKfycbwdtw-0f-cucTO7XFCVeCuLLiof5b9mmu5U89tu1w-xabkNNy8Dv-6vso5R6A60u2tI/exec";
 //const GAS_URL13 = "https://script.google.com/macros/s/AKfycbxMBCCxug018E3fb9V0dEVGRd2_wjwfliGkcRqN0olQVqPEcpk-8knmVQoMA6hW6P15/exec";
 const GAS_URL = "https://script.google.com/macros/s/AKfycbxTBPqHIilhjObUQ1o67MnA1j0owBWrqXriGETDcikG2KzN7ufVtkeCuLzqs300lylO/exec";
+const CF_URL = "https://us-central1-kuronekotaiwan-2025.cloudfunctions.net/insertToBigQuery";
 
 
-export const postStamp = (userId: string, stampId: string) => {
+export const postStamp_GAS = (userId: string, stampId: string) => {
     //const data = {
     //    uuid: userId,
     //    stamp_id: stampId
@@ -27,3 +28,20 @@ export const postStamp = (userId: string, stampId: string) => {
       .catch(err => console.error(err));
 }
 
+export const postStamp = (userId: string, stampId: string) => {
+  const data = {
+      uuid: userId,
+      stamp_id: stampId
+  };
+  fetch(CF_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data),
+      mode: "cors"
+    })
+    .then(res => res.text())
+    .then(text => console.log(text))
+    .catch(err => console.error(err));
+}
