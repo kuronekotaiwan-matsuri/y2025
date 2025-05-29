3a48475cd1a9ffd67f308e16f15f85bc87978a39
yo16 <xxyo1xx@gmail.com>
2025-05-29 17:24:08 +0900

    古いappを削除

--- app-website/src/declarations.d.ts
+++
@@ -1,4 +0,0 @@
-// src/declarations.d.ts
-declare module "swiper/css";
-declare module "swiper/css/navigation";
-declare module "swiper/css/pagination";
--- app-website/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-//import tailwindcss from '@tailwindcss/vite'  // これを使うと、一部のtailwindcssの設定が反映されないので使用しない
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/y2025/',
-})
